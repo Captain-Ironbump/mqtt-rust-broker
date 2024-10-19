bd4beb14cc96a940126b29f811c837e4cdd77fcf
Lukas Linde <linde.lukas.99@gmail.com>
2024-10-20 00:02:47 +0200

    will_topic variable extracted from payload data

--- src/models/mqtt_payloads.rs
+++ src/models/mqtt_payloads.rs
@@ -35,14 +35,17 @@ impl PayloadFactory {
             let mut payload_idx: usize = 2 as usize;
             let client_id: String = String::from_utf8(payload_data[payload_idx..client_id_length + payload_idx].to_vec()).unwrap();
             payload_idx += client_id_length + 1; // +1 to skip the length of the client id
-            let mut will_topic: Option<String> = None;
+            let mut will_topic: String = "".to_string();
             if connect_header.connect_flags & 0b00000100 != 0 {
-                
+                let will_topic_length: usize = (payload_data[payload_idx] as usize) << 8 | payload_data[payload_idx + 1] as usize;
+                payload_idx += 2;
+                will_topic = String::from_utf8(payload_data[payload_idx..will_topic_length + payload_idx].to_vec()).unwrap();    
             }
             
             
             Payload {
                 client_id: Some(client_id),
+                will_topic: Some(will_topic),
                 ..Default::default()
             }
         } else {
